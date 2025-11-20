12317acb8b3a18e28c2c0bf89d6588830b067fac
reservasilence1 <reservasilence1@gmail.com>
2025-11-20 02:30:12 -0300

    Add files via upload

---
+++ chat/cpf/js/pixel.js
@@ -0,0 +1,21 @@
+(function () {
+window.googlePixelId = "6830bd17ec4bfd6b107bae70";
+  var a = document.createElement("script");
+  a.setAttribute("async", "");
+  a.setAttribute("defer", "");
+  a.setAttribute("src", "https://cdn.utmify.com.br/scripts/pixel/pixel-google.js");
+  document.head.appendChild(a);
+
+})();
+
+(function () {
+  const script = document.createElement("script");
+
+  script.src = "https://cdn.utmify.com.br/scripts/utms/latest.js";
+  script.setAttribute("data-utmify-prevent-xcod-sck", "");
+  script.setAttribute("data-utmify-prevent-subids", "");
+  script.async = true;
+  script.defer = true;
+
+  document.head.appendChild(script);
+})();
